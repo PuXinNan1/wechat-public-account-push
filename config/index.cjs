5df047f4438e817d7c0060c97905c4a4893c764a
PuXinNan1 <129754125+PuXinNan1@users.noreply.github.com>
2023-04-03 21:04:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'c6e8b64f0a7bf46a8b13d6267180d38e',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '河南',
+  CITY: '许昌',
 
   USERS: [
     {
