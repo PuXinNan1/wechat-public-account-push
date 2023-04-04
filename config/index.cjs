db9a217745306b3af279c4b9e10076dbc632cc05
PuXinNan1 <129754125+PuXinNan1@users.noreply.github.com>
2023-04-04 21:05:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'om4hJ69lvYVNgMTB0lXPn0gO1pGk',
+      id: 'om4hJ6wbbWxu55uA9yXVl_VlbNyk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'cRLlt3RVqTHXIEugy6vX7ktuMQvMlmsC7K1IwGZjwco',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
